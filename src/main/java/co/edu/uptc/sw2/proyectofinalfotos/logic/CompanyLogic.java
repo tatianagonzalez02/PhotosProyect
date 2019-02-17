f212d7ef08a688c7a80e1724f9eefaac8fcdf8f6
FREDY <FREDY@Adwin7tecno20>
2019-02-17 14:27:59 -0500

    haciendo prueba

---
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/logic/CompanyLogic.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uptc.sw2.proyectofinalfotos.logic;
+
+import co.edu.uptc.sw2.proyectofinalfotos.entities.Company;
+import co.edu.uptc.sw2.proyectofinalfotos.persistence.CompanyDAO;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author FREDY
+ */
+@Stateless
+public class CompanyLogic {
+    
+    @EJB
+    private CompanyDAO companyDAO;
+    
+    public void addCompany(Company company){
+        companyDAO.addCompany(company);
+    }
+}
---
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/persistence/CompanyDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uptc.sw2.proyectofinalfotos.persistence;
+
+import co.edu.uptc.sw2.proyectofinalfotos.entities.Company;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author FREDY
+ */
+@Stateless
+public class CompanyDAO {
+ 
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    public void addCompany(Company company){
+        entityManager.persist(company);
+    }
+}
