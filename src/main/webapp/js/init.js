29140f6d0b540a32682b9f2c4068137d4c7de290
Karol Tatiana Alfonso G. <karol.alfonso@uptc.edu.co>
2019-02-18 20:34:45 -0500

    Correcciones visuales

--- src/main/webapp/js/init.js
+++ src/main/webapp/js/init.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+     var elemsP = document.querySelectorAll('.parallax');
+    var instancesP = M.Parallax.init(elemsP, {});
+//  });
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {});
     $('.carousel').carousel({
