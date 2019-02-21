c1dea08ab457410f75bbca0701aa852694e1a12a
fredygamba <fredygamba@hotmail.es>
2019-02-20 22:19:40 -0500

    Este no tiene cambios

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/persistence/CompanyDAO.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/persistence/CompanyDAO.java
@@ -3,7 +3,6 @@ package co.edu.uptc.sw2.proyectofinalfotos.persistence;
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Company;
 import com.google.gson.Gson;
 import errors.ErrorCompany;
-import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -42,6 +41,4 @@ public class CompanyDAO {
         }
         return GSON.toJson(ERROR_COMPANY);
     }
-    
-    
 }
