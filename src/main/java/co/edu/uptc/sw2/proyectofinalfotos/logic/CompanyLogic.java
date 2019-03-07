9b7cbe7aa206449252bfa1fb11115f5cd9c05d04
John Jimenez <john.jimenez02@uptc.edu.co>
2019-03-07 14:02:32 -0500

    Base de datos

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/logic/CompanyLogic.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/logic/CompanyLogic.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.uptc.sw2.proyectofinalfotos.logic;
 
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Company;
@@ -12,10 +7,6 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-/**
- *
- * @author FREDY
- */
 @Stateless
 public class CompanyLogic {
     
--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/services/CompanyService.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/services/CompanyService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.uptc.sw2.proyectofinalfotos.services;
 
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Company;
@@ -14,10 +9,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-/**
- *
- * @author FREDY
- */
 @Path("CompanyService")
 public class CompanyService {
     
