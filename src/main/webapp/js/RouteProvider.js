d249666974d825bb4be741c11aafba099a991790
fredygamba <fredygamba@hotmail.es>
2019-03-06 21:52:50 -0500

    Se le asigno un controlador propio para el html listProyects.html

--- src/main/webapp/js/RouteProvider.js
+++ src/main/webapp/js/RouteProvider.js
@@ -6,7 +6,7 @@ app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/companies', {templateUrl: "partials/companies.html", controller: "ctrlCompanies"});
         $routeProvider.when('/login', {templateUrl: "partials/login.html", controller: "ctrlLogin"});
         $routeProvider.when('/proyects', {templateUrl: "partials/proyects.html", controller: "ctrlProyects"});
-        $routeProvider.when('/listProyects', {templateUrl: "partials/listProyects.html", controller: "ctrlCompanies"});
+        $routeProvider.when('/listProyects', {templateUrl: "partials/listProyects.html", controller: "ctrlProjectsViewer"});
         $routeProvider.when('/register', {templateUrl: "partials/register.html", controller: "ctrlRegister"});
         $routeProvider.otherwise({redirectTo: '/'});
 
