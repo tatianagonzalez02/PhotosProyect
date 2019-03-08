3a3b42a77da243cea522e15f98ba55ee4297fe1c
fredygamba <fredygamba@hotmail.es>
2019-03-07 20:57:20 -0500

    Modificaciones para listar las fotos del proyecto elegido.

--- src/main/webapp/js/ProyectsController.js
+++ src/main/webapp/js/ProyectsController.js
@@ -1,21 +1,29 @@
 app.controller("ctrlProyects", function ($scope, $http) {
 
     $scope.company = null;
+    $scope.projects = null;
+    $scope.photosToShow = null;
     
     $scope.getCompany = function (){
         $http.get("./webresources/CompanyService/" + localStorage.getItem("company"),{})
             .then(function(response) {
                 $scope.company = response.data;
+                $scope.projects = $scope.company.listProyects;
         }, function(){
             alert("Error al obtener compañias");
         });
     };
     
-    $scope.getCompany();
-    
     //    abre el modal donde muestra las fotografias
-    $scope.verListFotografias= function (){
+    $scope.verListFotografias= function (idProject){
+        for (var i = 0; i < $scope.projects.length; i++) {
+            if ($scope.projects[i].id === idProject) {
+                $scope.photosToShow = $scope.projects[i].listPhotos;
+                break;
+            }
+        }
          $('#modal2').modal().open();
     };
     
+    $scope.getCompany();
 });
