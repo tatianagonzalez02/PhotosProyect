e76789dfeaeeabdd0dc74bbc79fbd24f9f17799c
fredygamba <fredygamba@hotmail.es>
2019-03-07 20:44:45 -0500

    Modificaciones de algunos métodos para obtener las imagenes del proyecto elegido. Y otros cambios...

--- src/main/webapp/js/ProjectsViewerController.js
+++ src/main/webapp/js/ProjectsViewerController.js
@@ -1,24 +1,33 @@
 app.controller("ctrlProjectsViewer", function ($scope, $http) {
 
     $scope.company = null;
+    $scope.chosenProject = null;
+    $scope.photosToShow = null;
     
     $scope.getData = function (){
         $http.get("./webresources/CompanyService/" + localStorage.getItem("viewCompany"),{})
             .then(function(response) {
                 $scope.company = response.data;
+                $scope.projects = $scope.company.listProyects;
         }, function(){
             alert("Error al obtener compañias");
         });
     };
     
-    $scope.verListFotografias= function (idProyect){
-        console.log(idProyect);//Voy aqui...
+    $scope.verListFotografias= function (idProject){
+        for (var i = 0; i < $scope.projects.length; i++) {
+            if ($scope.projects[i].id === idProject) {
+                $scope.photosToShow = $scope.projects[i].listPhotos;
+                break;
+            }
+        }
          $('#modal1').modal().open();
     };
     
     //abre el modal para subir fotografias
-    $scope.subirFotografias= function (){
-         $('#modalSubirF').modal().open();
+    $scope.subirFotografias= function (idProject){
+        $scope.chosenProject = idProject;
+        $('#modalSubirF').modal().open();
     };
     
     $scope.sendPhoto = function (){
@@ -27,7 +36,7 @@ app.controller("ctrlProjectsViewer", function ($scope, $http) {
             console.log("File seleccionado: ", input.files[0]);
         }
         $http.post("./webresources/ProyectService/sendPhoto", input.files[0])
-    }
+    };
     
     $scope.getData();
 });
