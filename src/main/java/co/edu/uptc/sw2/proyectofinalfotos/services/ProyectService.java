bc8acf27816bc40f36c6ce178a7c47a739faafff
fredygamba <fredygamba@hotmail.es>
2019-03-10 09:49:24 -0500

    Ahora se genera un ID para las imagenes segun la ultima guardada.

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/services/ProyectService.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/services/ProyectService.java
@@ -4,12 +4,11 @@ import co.edu.uptc.sw2.proyectofinalfotos.entities.EnumStatus;
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Photo;
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Proyect;
 import co.edu.uptc.sw2.proyectofinalfotos.logic.ProjectLogic;
-import java.awt.image.BufferedImage;
+import co.edu.uptc.sw2.proyectofinalfotos.utils.FilesManager;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import javax.ejb.EJB;
-import javax.imageio.ImageIO;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -23,25 +22,17 @@ public class ProyectService {
     @POST
     @Path("/sendPhoto")
     public String sendPhoto(File image) throws IOException{
-        BufferedImage image1 = ImageIO.read(image);
-        File outputFile = new File("C:/Users/asus/desktop/mifoto.png");
-        ImageIO.write(image1, "png", outputFile);
-        return outputFile.getPath();
+        return FilesManager.saveImage(image);
     }
     
     @PUT
     @Path("/update")
     public void updateProject(Proyect proyect){
-        System.out.println(proyect);
         List<Photo> photos = proyect.getListPhotos();
         for (Photo photo : photos) {
             photo.setEnumStatus(EnumStatus.IN_PROCESS);
         }
         projectLogic.updateProject(proyect);
-//        List<Photo> photos = proyect.getListPhotos();
-//        for (Photo photo : photos) {
-//            System.out.println(photo.getPath());
-//        }
     }
     
 }
---
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/utils/FilesManager.java
@@ -0,0 +1,25 @@
+package co.edu.uptc.sw2.proyectofinalfotos.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class FilesManager {
+    
+    public static final String PATH = "C:/Users/FREDY/Downloads/img/";
+ 
+    public static String saveImage(File image) throws IOException{
+        String[] files = new File(PATH).list();
+        int lastID = 1;
+        if (files.length > 0) {
+            String file = files[files.length - 1];
+            lastID = Integer.parseInt(file.substring(0, file.length()  - 4)) + 1;
+        }
+        BufferedImage image1 = ImageIO.read(image);
+        File outputFile = new File(PATH + lastID + ".png");
+        ImageIO.write(image1, "png", outputFile);
+        return outputFile.getPath();
+    }
+    
+}
