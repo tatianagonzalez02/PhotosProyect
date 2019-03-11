0b398a9aa69d332c19dbffc09280a2d5a7fc7906
fredygamba <fredygamba@hotmail.es>
2019-03-10 22:43:39 -0500

    Nuevo NamedQuery.

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/entities/Photo.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/entities/Photo.java
@@ -6,8 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "getPhotos", query = "SELECT p FROM Photo p")
+})
 public class Photo implements Serializable {
 
     @Id
