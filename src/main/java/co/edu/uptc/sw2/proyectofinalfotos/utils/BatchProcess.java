dcb4756d2c9c63cd101f421330746a2dcc19c1d4
fredygamba <fredygamba@hotmail.es>
2019-03-13 13:07:51 -0500

    -Ya no obtiene todas las fotos, solo segun su estado. -Agrega texto a la imagen para el fotografo y la fecha. -Permite el control de excepciones.

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/utils/BatchProcess.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/utils/BatchProcess.java
@@ -7,8 +7,12 @@ package co.edu.uptc.sw2.proyectofinalfotos.utils;
 
 import co.edu.uptc.sw2.proyectofinalfotos.entities.EnumStatus;
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Photo;
+import co.edu.uptc.sw2.proyectofinalfotos.entities.Photographer;
 import co.edu.uptc.sw2.proyectofinalfotos.logic.PhotoLogic;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
@@ -20,23 +24,28 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class BatchProcess {
- 
+
     @EJB
     private PhotoLogic photoLogic;
-    
+
     private static final String PATH = "C:\\Users\\FREDY\\Downloads\\imgProcess\\";
-    
-    @Schedule(minute = "*", hour = "*", second = "0/10", persistent = false)
-    public void searchImages(){
-        System.out.println("Voy a buscar imagenes sin procesar...");
-        List<Photo> photos = photoLogic.getPhotos();
+
+    @Schedule(minute = "*", hour = "*", second = "0/15", persistent = false)
+    public void searchImages() {
+        System.out.println(new Date().getSeconds() + " Voy a buscar imagenes sin procesar...");
+        List<Photo> photos = photoLogic.getPhotos(EnumStatus.IN_PROCESS);
         for (Photo photo : photos) {
-            if (photo.getEnumStatus() == EnumStatus.IN_PROCESS) {
-                String path = photo.getPath();
+            try {
                 System.out.println("Encontre una foto sin procesar, es: " + photo);
-                ImageManager.copyImage(path, PATH + new File(path).getName());
+                String path = photo.getPath();
+                Photographer photographer = photo.getPhotographer();
+                ImageManager.copyImage(path, PATH + new File(path).getName(),
+                        photographer.getName() + " " + photographer.getLastName(),
+                        new SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(photo.getDate()));
                 photo.setEnumStatus(EnumStatus.AVAILABLE);
                 photoLogic.update(photo);
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
         }
         System.out.println("Ya termine de buscar imagenes");
