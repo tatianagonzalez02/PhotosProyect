6eb52a7f0d09d9615ba92059c274f169251e079e
fredygamba <fredygamba@hotmail.es>
2019-03-14 18:17:28 -0500

    Al agregar una foto, busca el fotografo.

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/logic/PhotoLogic.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/logic/PhotoLogic.java
@@ -2,6 +2,7 @@ package co.edu.uptc.sw2.proyectofinalfotos.logic;
 
 import co.edu.uptc.sw2.proyectofinalfotos.entities.EnumStatus;
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Photo;
+import co.edu.uptc.sw2.proyectofinalfotos.entities.Photographer;
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Proyect;
 import co.edu.uptc.sw2.proyectofinalfotos.persistence.PhotoDAO;
 import java.util.ArrayList;
@@ -18,6 +19,9 @@ public class PhotoLogic {
     
     @EJB
     private ProjectLogic projectLogic;
+    
+    @EJB
+    private PhotographerLogic photographerLogic;
     
     public List<Photo> getPhotos(){
         return photoDAO.getPhotos();
@@ -44,6 +48,10 @@ public class PhotoLogic {
 
     public void addPhoto(int idProject, Photo photo) {
         photo.setEnumStatus(EnumStatus.IN_PROCESS);
+        Photographer photographer = photographerLogic.getPhotographer(photo.getPhotographer().getEmailP());
+        if (photographer != null) {
+            photo.setPhotographer(photographer);
+        }
         Proyect proyect = projectLogic.getProject(idProject);
         if (proyect.getListPhotos() == null) {
             proyect.setListPhotos(new ArrayList<Photo>());
