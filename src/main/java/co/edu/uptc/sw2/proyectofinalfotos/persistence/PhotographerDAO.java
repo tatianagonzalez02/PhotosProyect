94add4609b9530d9602e70bed44897fae4569fa9
fredygamba <fredygamba@hotmail.es>
2019-03-14 18:47:44 -0500

    Solo le quite una linea.

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/persistence/PhotographerDAO.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/persistence/PhotographerDAO.java
@@ -6,7 +6,6 @@ import com.google.gson.Gson;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 @Stateless
 public class PhotographerDAO {
