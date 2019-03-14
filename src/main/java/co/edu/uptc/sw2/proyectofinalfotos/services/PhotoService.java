9cad99e0ba60b6a58edee12f50486583881b561d
fredygamba <fredygamba@hotmail.es>
2019-03-14 15:11:45 -0500

    Obtiene los fotos de PhotoLogic.

--- src/main/java/co/edu/uptc/sw2/proyectofinalfotos/services/PhotoService.java
+++ src/main/java/co/edu/uptc/sw2/proyectofinalfotos/services/PhotoService.java
@@ -2,7 +2,6 @@ package co.edu.uptc.sw2.proyectofinalfotos.services;
 
 import co.edu.uptc.sw2.proyectofinalfotos.entities.Photo;
 import co.edu.uptc.sw2.proyectofinalfotos.logic.PhotoLogic;
-import co.edu.uptc.sw2.proyectofinalfotos.logic.ProjectLogic;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
@@ -16,9 +15,6 @@ public class PhotoService {
     @EJB
     private PhotoLogic photoLogic;
     
-    @EJB
-    private ProjectLogic projectLogic;
-    
     @POST
     @Path("/{idProject}")
     public void addPhoto(@PathParam("idProject") int idProject, Photo photo){
@@ -28,6 +24,6 @@ public class PhotoService {
     @GET
     @Path("/{idProject}")
     public List<Photo> getPhotos(@PathParam("idProject") int idProject){
-        return projectLogic.getProject(idProject).getListPhotos();
+        return photoLogic.getPhotos(idProject);
     }
 }
